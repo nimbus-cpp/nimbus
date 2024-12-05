32c933fecdb591a27bcbc22f8607b3395172f3f5
Kasim Fejzic <kasimfejzo@gmail.com>
2024-12-05 02:30:40 +0100

    refactor: export `init` constants to a `init::constants` namespace containing constexpr string views; rename external function to `process`

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char **argv) {
 
   std::string projectName;
   initCmd->add_option("project_name", projectName, "Name of the project");
-  initCmd->callback([&]() { init::handle(projectName); });
+  initCmd->callback([&]() { init::process(projectName); });
   // init command section -- end
 
   CLI11_PARSE(app, argc, argv);
